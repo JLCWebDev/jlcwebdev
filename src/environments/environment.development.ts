6fa78aab8bcc51ad05ac151dcda798975468fa32
jevus09 <jevus09@gmail.com>
2024-08-27 12:34:17 -0500

    update env files to succesfully build

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    emailJSServiceId: process.env['emailJSServiceId'] || '',
+    emailJSTemplateId: process.env['emailJSTemplateId'] || '',
+    emailJSPublicKey: process.env['emailJSPublicKey'] || '',
+    production: process.env['NODE_ENV'] === 'production',
+  };
