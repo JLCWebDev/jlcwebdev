626d16b14a24fa7547ab6d354d170383465e1174
jevus09 <jevus09@gmail.com>
2024-08-27 12:47:35 -0500

    another test

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    emailJSServiceId: process.env['emailJSServiceId'] || '',
-    emailJSTemplateId: process.env['emailJSTemplateId'] || '',
-    emailJSPublicKey: process.env['emailJSPublicKey'] || '',
-    production: process.env['NODE_ENV'] === 'production',
+    production: true,
+    emailJSServiceId: '',
+    emailJSTemplateId: '',
+    emailJSPublicKey: '',
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,4 @@
 export const environment = {
-    emailJSServiceId: process.env['emailJSServiceId'] || '',
-    emailJSTemplateId: process.env['emailJSTemplateId'] || '',
-    emailJSPublicKey: process.env['emailJSPublicKey'] || '',
-    production: process.env['NODE_ENV'] === 'production',
+
   };
   
