39949b3c32e2611ee0c9904c15e50b4faab05510
jevus09 <jevus09@gmail.com>
2024-08-27 12:16:31 -0500

    fixed bug

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
+const path = './src/environments';
+
+// Ensures the environments directory exists
+if (!fs.existsSync(path)) {
+    fs.mkdirSync(path, { recursive: true });
+}
 
 const envConfigFile = `
 export const environment = {
   production: true,
   apiUrl: '${process.env.API_URL}',
-  // add more environment variables here
+  // Add other environment variables here
 };
 `;
 
-fs.writeFileSync('./src/environments/environment.prod.ts', envConfigFile);
+fs.writeFileSync(`${path}/environment.prod.ts`, envConfigFile);
